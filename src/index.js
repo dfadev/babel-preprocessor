e689192ccd0b8b50fc58d37a83e37095d452e7cb
Russ Panula <dfadev@dfa.net>
2017-10-08 21:33:57 -0500

    add $INCLUDE, PROCINCLUDE, $PROCINCLUDE

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import * as fs from "fs";
 import * as babylon from "babylon";
 import * as babel from "babel-core";
 import * as p from "path";
+import { spawnSync } from 'child_process';
 
 const PREP = "PREP", PREPROCESS = "PREPROCESS", END = "END",
-	INCLUDE = "INCLUDE", DEFINE = "DEFINE",
+	INCLUDE = "INCLUDE", STRINCLUDE = "$INCLUDE", DEFINE = "DEFINE",
+	PROCINCLUDE = "PROCINCLUDE", STRPROCINCLUDE = "$PROCINCLUDE",
 	IF = "IF", ELSEIF = "ELSEIF", ELSE = "ELSE";
 
 function evalAst(node) { return evalGlobal(generate(node).code); }
@@ -28,6 +30,7 @@ export default function({types: t }) {
 					var nm = path.node.expression.callee.name;
 					if (nm === PREPROCESS || nm === PREP) handlePrepCall(path, state);
 					else if (nm === INCLUDE) handleInclude(path, state);
+					else if (nm === PROCINCLUDE) handleProcInclude(path, state);
 					else if (nm === DEFINE) handleDefine(path, state);
 					else if (nm === IF) handleIf(path, state);
 					else if (nm === ELSEIF) handleElseIf(path, state);
@@ -37,6 +40,15 @@ export default function({types: t }) {
 			MemberExpression(path, state) {
 				if (t.isIdentifier(path.node.object) && (path.node.object.name == PREP || path.node.object.name == PREPROCESS))
 					handlePrepAccess(path, state);
+			},
+
+			CallExpression(path, state) {
+				if (t.isIdentifier(path.node.callee)) {
+					var nm = path.node.callee.name;
+
+					if (nm === STRINCLUDE) handleStringInclude(path, state);
+					else if (nm === STRPROCINCLUDE) handleStringProcessInclude(path, state);
+				}
 			}
 		}
 	};
@@ -120,6 +132,16 @@ function handlePrepCall(path, state) {
 	path.remove();
 }
 
+function insertCode(path, state, code) {
+	var rslt = babel.transform(code);
+	for (let i = 0; i < rslt.ast.program.body.length; i++) {
+		if (state.current && state.current.prep)
+			state.current.code.push(rslt.ast.program.body[i]);
+		else
+			path.insertBefore(rslt.ast.program.body[i]);
+	}
+}
+
 function handleInclude(path, state) {
 	if ( !state.current || (state.current && state.current.satisfied && !state.current.remove) ) {
 		var inc = "";
@@ -130,15 +152,53 @@ function handleInclude(path, state) {
 			inc += fs.readFileSync(fname).toString();
 		}
 
-		var rslt = babel.transform(inc);
-		for (let i = 0; i < rslt.ast.program.body.length; i++) {
-			if (state.current && state.current.prep)
-				state.current.code.push(rslt.ast.program.body[i]);
-			else
-				path.insertBefore(rslt.ast.program.body[i]);
-		}
+		insertCode(path, state, inc);
 	}
 	path.remove();
+}
+
+function handleStringInclude(path, state) {
+	if ( !state.current || (state.current && state.current.satisfied && !state.current.remove) ) {
+		var inc = "";
+		for (let i = 0; i < path.node.arguments.length; i++) {
+			var fname = evalAst(path.node.arguments[i]);
+			if (!fs.existsSync(fname))
+				throw path.buildCodeFrameError("missing include file: " + p.join(process.cwd(), fname));
+			inc += fs.readFileSync(fname).toString();
+		}
+
+		path.replaceWith(t.stringLiteral(inc));
+	}
+}
+
+function handleProcInclude(path, state) {
+	if ( !state.current || (state.current && state.current.satisfied && !state.current.remove) ) {
+		var cmd = "", args = [], opts = {};
+		var len = path.node.expression.arguments.length;
+		if (len > 0) cmd = evalAst(path.node.expression.arguments[0]);
+		if (len > 1) args = eval(generate(path.node.expression.arguments[1]).code);
+		if (len > 2) opts = eval('(' + generate(path.node.expression.arguments[2]).code + ')');
+
+		var rslt = spawnSync(cmd, args, opts);
+		var rsltStr = rslt.stdout ? rslt.stdout.toString() : rslt.stderr ? rslt.stderr.toString() : "";
+
+		insertCode(path, state, rsltStr);
+	}
+	path.remove();
+}
+
+function handleStringProcessInclude(path, state) {
+	if ( !state.current || (state.current && state.current.satisfied && !state.current.remove) ) {
+		var cmd = "", args = [], opts = {};
+		var len = path.node.arguments.length;
+		if (len > 0) cmd = evalAst(path.node.arguments[0]);
+		if (len > 1) args = eval(generate(path.node.arguments[1]).code);
+		if (len > 2) opts = eval('(' + generate(path.node.arguments[2]).code + ')');
+
+		var rslt = spawnSync(cmd, args, opts);
+		var rsltStr = rslt.stdout ? rslt.stdout.toString() : rslt.stderr ? rslt.stderr.toString() : "";
+		path.replaceWith(t.stringLiteral(rsltStr));
+	}
 }
 
 function handleDefine(path, state) {
